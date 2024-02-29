b45cd9001663670fb422b2d29e07bf0fd3991f6d
Apoorv Kothari <apoorv@toidiu.com>
2024-02-29 11:38:21 -0800

    feat(orchestrator): include cli parse for the orchestrator

--- netbench-orchestrator/src/ec2_utils.rs
+++ netbench-orchestrator/src/ec2_utils.rs
@@ -19,3 +19,33 @@ impl std::fmt::Display for PubIp {
         write!(f, "{}", self.0)
     }
 }
+
+macro_rules! ec2_new_types {
+    ($name:ident) => {
+        #[derive(Clone, Debug, Hash, Eq, PartialEq, PartialOrd, Ord)]
+        pub struct $name(String);
+
+        impl $name {
+            pub fn into_string(self) -> String {
+                self.clone().0
+            }
+        }
+
+        impl From<String> for $name {
+            fn from(value: String) -> Self {
+                $name(value)
+            }
+        }
+
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}: {}", std::any::type_name::<$name>(), self.0)?;
+                Ok(())
+            }
+        }
+    };
+}
+
+ec2_new_types!(SubnetId);
+ec2_new_types!(VpcId);
+ec2_new_types!(Az);
--- netbench-orchestrator/src/main.rs
+++ netbench-orchestrator/src/main.rs
@@ -3,8 +3,11 @@
 
 // TODO remove
 #![allow(dead_code)]
-
-use crate::orchestrator::OrchResult;
+use crate::orchestrator::{OrchResult, RunMode, STATE};
+use aws_config::BehaviorVersion;
+use aws_types::region::Region;
+use clap::Parser;
+use tracing_subscriber::EnvFilter;
 
 mod ec2_utils;
 mod orchestrator;
@@ -13,5 +16,29 @@ mod ssm_utils;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> OrchResult<()> {
-    Ok(())
+    let unique_id = format!(
+        "{}-{}",
+        humantime::format_rfc3339_seconds(std::time::SystemTime::now()),
+        STATE.version
+    );
+
+    let file_appender =
+        tracing_appender::rolling::daily("./target", format!("russula_{}", unique_id));
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_writer(non_blocking)
+        .init();
+
+    let cli = orchestrator::Cli::parse().process_config_files()?;
+    let region = Region::new(cli.region());
+    let aws_config = aws_config::defaults(BehaviorVersion::v2023_11_09())
+        .region(region)
+        .load()
+        .await;
+
+    // perform sanity and check before proceeding
+    let config = cli.check_requirements(&aws_config).await?;
+
+    orchestrator::run(unique_id, &config, &aws_config, RunMode::Full).await
 }
