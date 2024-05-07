a062af7c0d80b5044b8373d00e3db9694cf9bd4c
Apoorv Kothari <apoorv@toidiu.com>
2024-05-06 17:08:21 -0700

    dont commit req_resp file

--- netbench-orchestrator/scripts/net_client.sh
+++ netbench-orchestrator/scripts/net_client.sh
@@ -5,4 +5,4 @@
 
 set -e
 
-PORT=9000 SERVER_0=127.0.0.1:7001 SERVER_1=127.0.0.1:8001 ../target/release/s2n-netbench-collector ../target/release/s2n-netbench-driver-client-s2n-quic --scenario scripts/request_response_multi_2_incast_1GB_req_resp.json
+PORT=9000 SERVER_0=127.0.0.1:7001 SERVER_1=127.0.0.1:8001 ../target/release/s2n-netbench-collector ../target/release/s2n-netbench-driver-client-s2n-quic --scenario ../target/s2n-netbench/request_response.json
